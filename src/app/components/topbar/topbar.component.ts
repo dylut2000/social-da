1f2023437032c703af7e3613b937b3d5ffc96724
Hardy Lutula <hardylutula@gmail.com>
2021-06-28 11:26:49 +0200

    topbar done

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-topbar',
+  selector: 'Topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
