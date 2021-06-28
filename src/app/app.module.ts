1f899cf4f8521da9e8db81fc4446864bc9dafe93
Hardy Lutula <hardylutula@gmail.com>
2021-06-28 10:17:21 +0200

    starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { RightbarComponent } from './components/rightbar/rightbar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingComponent,
+    ProfileComponent,
+    SidebarComponent,
+    TopbarComponent,
+    RightbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/rightbar/rightbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rightbar',
+  templateUrl: './rightbar.component.html',
+  styleUrls: ['./rightbar.component.scss']
+})
+export class RightbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
