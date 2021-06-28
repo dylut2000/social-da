b11bd93c33e27ef9b363c96d8abbd01bac4463c1
Hardy Lutula <hardylutula@gmail.com>
2021-06-28 12:41:27 +0200

    we will continue later

--- src/app/components/rightbar/rightbar.component.ts
+++ src/app/components/rightbar/rightbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rightbar',
+  selector: 'Rightbar',
   templateUrl: './rightbar.component.html',
   styleUrls: ['./rightbar.component.scss']
 })
